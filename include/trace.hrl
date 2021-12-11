f3c2042d8abf486e6afccf08a5d2efb3fc0dfee4
k32 <10274441+k32@users.noreply.github.com>
2021-12-11 21:47:11 +0100

    Allow to customize the behavior of the trace points

--- include/trace.hrl
+++ include/trace.hrl
@@ -1,17 +1,21 @@
 -ifndef(SNABBKAFFE_TRACE_HRL).
 -define(SNABBKAFFE_TRACE_HRL, true).
 
--ifdef(TEST).
 -ifndef(SNK_COLLECTOR).
--define(SNK_COLLECTOR, true).
--endif. %% SNK_COLLECTOR
--endif. %% TEST
+  -ifdef(TEST).
+    -define(SNK_COLLECTOR, true).
+  -else.
+    -define(SNK_COLLECTOR, false).
+  -endif.
+-endif.
 
--ifdef(SNK_COLLECTOR).
--include("trace_test.hrl").
--else. %% SNK_COLLECTOR
--include("trace_prod.hrl").
--endif. %% SNK_COLLECTOR
+-if(?SNK_COLLECTOR == true).
+  -include("trace_test.hrl").
+-elif(?SNK_COLLECTOR == false).
+  -include("trace_prod.hrl").
+-else.
+  -error("SNK_COLLECTOR macro should be defined as either true or false").
+-endif.
 
 %% TODO: I don't like the overhead of having a fun here in the prod
 %% mode, but removing it can change the semantics of the program in
