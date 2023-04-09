1cd79b413db364e5435e86163a9b993c11bf6f32
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2023-04-09 14:46:53 +0200

    WIP: fixed it

--- src/snabbkaffe_parser.erl
+++ src/snabbkaffe_parser.erl
@@ -39,8 +39,8 @@
 -type s(S, R) :: #stateful{val :: R, state :: S}.
 
 -opaque m(S, R) :: fun((S) ->
-                          {more, fun((_Event) -> s(S, R))} |
-                          {done, s(S, R)}).
+                          s(S, {more, fun((_Event) -> m(S, R))} |
+                               {done, R})).
 
 %%================================================================================
 %% API funcions
@@ -49,10 +49,11 @@
 -spec next() -> m(_S, snabbkaffe:event()).
 next() ->
   fun(State) ->
-      {more,
-       fun(Event) ->
-           #stateful{state = State, val = Event}
-       end}
+      #stateful{ state = State
+               , val = {more, fun(Event) ->
+                                  Event
+                              end}
+               }
   end.
 
 %% -spec discard() -> m(undefined).
@@ -66,22 +67,27 @@ next() ->
 -spec return(A) -> m(_S, A).
 return(A) ->
   fun(S) ->
-      {done, #stateful{ val = A
-                      , state = S
-                      }}
+      #stateful{ state = S
+               , val = {done, A}
+               }
   end.
 
 -spec bind(m(S, A), fun((A) -> m(S, B))) -> m(S, B).
 bind(Prev, Next) ->
   fun(State0) ->
       case Prev(State0) of
-        {done, #stateful{val = Val, state = State1}} ->
+        #stateful{val = {done, Val}, state = State1} ->
           (Next(Val))(State1);
-        {more, Cont} ->
-          {more, fun(Event) ->
-                     #stateful{state = State1, val = Val} = Cont(Event),
-                     (Next(Val))(State1)
-                 end}
+        #stateful{val = {more, Cont}, state = State1} ->
+          More = fun(Event) ->
+                     fun(State2) ->
+                         Val = Cont(Event),
+                         (Next(Val))(State2)
+                     end
+                 end,
+          #stateful{ state = State1
+                   , val = {more, More}
+                   }
       end
   end.
 
@@ -131,42 +137,62 @@ nomatch(A) ->
 -spec get_state() -> m(S, S).
 get_state() ->
   fun(S) ->
-      {done, #stateful{val = S, state = S}}
+      #stateful{val = {done, S}, state = S}
   end.
 
 -spec put_state(S) -> m(S, S).
 put_state(S) ->
   fun(_) ->
-      {done, #stateful{val = S, state = S}}
+      #stateful{val = {done, S}, state = S}
   end.
 
 -spec step(snabbkaffe:event(), #s{}) -> #s{}.
 step(Event, S = #s{counter = Id}) ->
   S.
 
-bind_00_test() ->
+bind_000_test() ->
+  M = [do/?MODULE ||
+        return(1),
+        return(2)],
+  S0 = state0,
+  ?assertMatch(#stateful{state = S0, val = {done, 2}}, M(S0)).
+
+bind_001_test() ->
+  M = [do/?MODULE ||
+        S0 <- get_state(),
+        put_state(1),
+        S1 <- get_state(),
+        put_state(2),
+        S2 <- get_state(),
+        return({S0, S1, S2})],
+  S0 = 0,
+  ?assertMatch(#stateful{state = 2, val = {done, {0, 1, 2}}}, M(S0)).
+
+bind_010_test() ->
   M = [do/?MODULE ||
         A <- next(),
         return(A)],
-  State0 = state1,
+  S0 = state0,
   Event0 = event1,
-  {more, M1} = M(State0),
-  ?assertMatch({done, #stateful{val = Event0, state = State0}}, M1(Event0)).
+  #stateful{state = S0, val = {more, M1}} = M(S0),
+  ?assertMatch(#stateful{val = {done, Event0}, state = S0},
+               (M1(Event0))(S0)).
 
-bind_01_test() ->
+bind_020_test() ->
   M = [do/?MODULE ||
         A <- next(),
         C <- return(1),
         X = A + C,
         B <- next(),
         return({X, B})],
-  State0 = 0,
-  {more, M1} = M(State0),
-  {more, M2} = M1(3),
-  {done, Ret} = M2(2),
-  ?assertMatch(#stateful{val = {4, 2}, state = State0}, Ret).
+  S0 = 0,
+  #stateful{state = S1, val = {more, M1}} = M(S0),
+  #stateful{state = S2, val = {more, M2}} = (M1(3))(S1),
+  #stateful{state = S3, val = {done, Ret}} = (M2(2))(S1),
+  ?assertMatch({4, 2}, Ret),
+  ?assertMatch(S0, S3).
 
-bind_02_test() ->
+bind_030_test() ->
   M = [do/?MODULE ||
         A <- next(),
         put_state([A]),
@@ -174,8 +200,7 @@ bind_02_test() ->
         S <- get_state(),
         put_state([B|S]),
         return(ok)],
-  State0 = [],
-  {more, M1} = M(State0),
-  {more, M2} = M1(3),
-  {done, Ret} = M2(2),
-  ?assertMatch(#stateful{val = ok, state = [2, 3]}, Ret).
+  S0 = [],
+  #stateful{state = S0, val = {more, M1}} = M(S0),
+  #stateful{state = S1 = [evt1], val = {more, M2}} = (M1(evt1))(S0),
+  #stateful{state = S2 = [evt2, evt1], val = {done, ok}} = (M2(evt2))(S1).
