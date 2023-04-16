442c784644a0cb1f590a528cacf33b0df944a1a2
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2023-04-16 23:11:28 +0200

    Second attempt to feed and seed

--- src/snabbkaffe_parser.erl
+++ src/snabbkaffe_parser.erl
@@ -74,7 +74,6 @@
         { parser_id :: parser_id()
         , prev_vertex :: vertex_id() | undefined
         , cont :: m(_, _)
-        , label :: label()
         }).
 
 %% Global state:
@@ -82,7 +81,6 @@
         { counter = 1 :: vertex_id()
         , vertices = #{} :: #{vertex_id() => map()}
         , edges = #{} :: edges()
-        , seed_parser :: m(_, _)
         , active = {[], []} :: zipper(#p{})
         , done = [] :: [{vertex_id(), term()}]
         , failed = [] :: [#p{}]
@@ -96,13 +94,29 @@
 
 %% @doc Suspend execution of the parser until the next token is available.
 %% Return a continuation and a label
--spec next(term()) -> m(_S, snabbkaffe:event()).
+-spec next(label()) -> m(state(), snabbkaffe:event()).
 next(Label) ->
-  fun(State) ->
-      #stateful{ state = State
-               , val = {more, Label, fun(Event) -> Event end}
-               }
-  end.
+  [do/?MODULE ||
+    {Cont, Evt} <- consume(),
+    %% FIXME: we first update the state (which can be a rather heavy
+    %% operation), and only then check if event actually matches. This
+    %% should be optimized.
+    over_state(fun(State0 = #s{active = Active, counter = Incident, edges = Edges}) ->
+                   {true, #p{ parser_id = ParserId
+                            , prev_vertex = Emanent
+                            } = P0} = zip_get(Active),
+                   Edge = #{ incident => Incident
+                           , label => Label
+                           , parser_id => ParserId
+                           , function => Cont
+                           },
+                   P = P0#p{cont = Cont, prev_vertex = Incident},
+                   State = State0#s{ edges  = add_edge(Emanent, Edges, Edge)
+                                   , active = zip_replace(P, Active)
+                                   },
+                   {undefined, State}
+               end),
+    return(Evt)].
 
 %% @equiv next(undefined)
 -spec next() -> m(_S, snabbkaffe:event()).
@@ -113,11 +127,9 @@ next() ->
 -spec fork() -> m(_S, undefined).
 fork() ->
   over_state(fun(S = #s{active = Active0}) ->
-                 1 = S,
-                 {true, Parser0 = #p{parser_id = Id}} = zip_get(Active0),
-                 Parser1 = Parser0#p{parser_id = [make_ref()|Id]},
-                 Parser2 = Parser0#p{parser_id = [make_ref()|Id]},
-                 Active = zip_insertl(Parser1, zip_replace(Parser2, Active0)),
+                 {true, OldParser = #p{parser_id = Id}} = zip_get(Active0),
+                 NewParser = OldParser#p{parser_id = [make_ref()|Id]},
+                 Active = zip_insertl(OldParser, zip_replace(NewParser, Active0)),
                  {undefined, S#s{active = Active}}
              end).
 
@@ -126,47 +138,23 @@ parse(Parser, Trace) ->
   complete(lists:foldl(fun feed/2, new(Parser), Trace)).
 
 -spec new(_SeedParser :: m(state(), _Result)) -> state().
-new(Seed) ->
-  #s{seed_parser = Seed}.
+new(Parser) ->
+  Seed = #p{ parser_id = []
+           , cont      = [do/?MODULE || fork(), Parser]
+           },
+  #s{active = zip_from_list([Seed])}.
 
 %% @doc Feed next event to the parser
 -spec feed(snabbkaffe:event(), state()) -> state().
-feed(Event, S1 = #s{counter = Id, seed_parser = Seed}) ->
-  %% Seed and feed:
-  #stateful{state = S2, val = {more, SeedLabel, Seed1}} = Seed(S1),
-  ParserId = [make_ref()],
-  PrevVertex = ParserId,
-  P = #p{ cont = Seed1
-        , label = SeedLabel
-        , parser_id = ParserId
-        , prev_vertex = PrevVertex
-        },
-  {Matched0, S4} =
-    case feed_current(Event, S2, P) of
-      {done, Done, S3 = #s{done = Done0, edges = Edges}, Edge} ->
-        {true, S3#s{ done = [Done | Done0]
-                   , edges = add_edge(PrevVertex, Edges, Edge)
-                   }};
-      {more, Next, S3 = #s{active = Active0, edges = Edges}, Edge} ->
-        {true, S3#s{ active = zip_insertl(Next, Active0)
-                   , edges = add_edge(PrevVertex, Edges, Edge)
-                   }};
-      {fail, Reason, S3 = #s{edges = Edges, failed = Failed}, Edge} ->
-        {true, S3#s{ failed = [Reason | Failed]
-                   , edges = add_edge(PrevVertex, Edges, Edge)
-                   }};
-      nomatch ->
-        {false, S1}
-    end,
-  %% Process the existing parsers:
-  {Matched, S = #s{active = Active, vertices = Vertices}} = do_feed(Matched0, Event, S4),
-  S#s{ active = zip_from_list(zip_to_list(Active))
-     , counter = Id + 1
-     , vertices = case Matched of
-                    true -> Vertices#{Id => Event};
-                    false -> Vertices %% Skip vertices that were ignored by all parsers
-                  end
-     }.
+feed(Event, S0 = #s{counter = Incident, vertices = Vertices}) ->
+  {Matched, S1} = do_feed(false, Event, S0),
+  S = S1#s{ counter = Incident + 1
+          , active  = zip_from_list(zip_to_list(S1#s.active))
+          },
+  case Matched of
+    true  -> S#s{vertices = Vertices#{Incident => Event}};
+    false -> S
+  end.
 
 %% @doc Finish parsing
 -spec complete(state()) -> parse_result().
@@ -235,7 +223,7 @@ bind(Prev, Next) ->
       case Prev(State0) of
         #stateful{val = {done, Val}, state = State1} ->
           (Next(Val))(State1);
-        #stateful{val = {more, Label, Cont}, state = State1} ->
+        #stateful{val = {more, Cont}, state = State1} ->
           More = fun(Event) ->
                      fun(State2) ->
                          Val = Cont(Event),
@@ -243,7 +231,7 @@ bind(Prev, Next) ->
                      end
                  end,
           #stateful{ state = State1
-                   , val = {more, Label, More}
+                   , val = {more, More}
                    }
       end
   end.
@@ -258,6 +246,14 @@ nomatch(A) ->
 %%================================================================================
 %% Internal functions
 %%================================================================================
+
+-spec consume() -> m(S, {m(S, R), R}).
+consume() ->
+  fun(State) ->
+      #stateful{ state = State
+               , val = {more, fun(Val) -> Val end}
+               }
+  end.
 
 %% @private Low level API that returns parser state
 -spec get_state() -> m(S, S).
@@ -280,67 +276,39 @@ over_state(Fun) ->
       #stateful{val = {done, Val}, state = State}
   end.
 
--spec do_feed(boolean(), snabbkaffe:event(), #s{}) -> {boolean(), #s{}}.
-do_feed(Matched0, Event, S0 = #s{active = Active0}) ->
+-spec do_feed(boolean(), snabbkaffe:event(), state()) -> {boolean(), state()}.
+do_feed(Matched, Event, S0 = #s{active = Active0}) ->
   case zip_get(Active0) of
     false ->
-      {Matched0, S0};
-    {true, Parser = #p{prev_vertex = Emanent}} ->
-      {Matched, S} =
-        case feed_current(Event, S0, Parser) of
-          {done, Result, S1 = #s{active = Active, done = Results, edges = Edges}, NewEdge} ->
-            {true, S1#s{ active = zip_remove(Active)
-                       , done   = [Result | Results]
-                       , edges  = add_edge(Emanent, Edges, NewEdge)
-                       }};
-          {more, Next, S1 = #s{active = Active, edges = Edges}, NewEdge} ->
-            {true, S1#s{ active = zip_replace(Next, Active)
-                       , edges  = add_edge(Emanent, Edges, NewEdge)
-                       }};
-          {fail, Reason, S1 = #s{active = Active, failed = Failed, edges = Edges}, NewEdge} ->
-            {true, S1#s{ active = zip_remove(Active)
-                       , failed = [Reason|Failed]
-                       , edges  = add_edge(Emanent, Edges, NewEdge)
-                       }};
-          nomatch ->
-            {Matched0, S0}
-        end,
-      do_feed(Matched, Event, S#s{active = zip_shiftr(S#s.active)})
+      %% Reached the end of the zipper:
+      {Matched, S0};
+    {true, P0} ->
+      {MatchedThis, S1} = feed_current(Event, S0, P0),
+      S = S1#s{active = zip_shiftr(S1#s.active)},
+      do_feed(Matched orelse MatchedThis, Event, S)
   end.
 
--spec feed_current(snabbkaffe:event(), #s{}, #p{}) -> {more, #p{}, #s{}, edge()}
-                                                    | {done, _Result, #s{}, edge()}
-                                                    | {fail, failed(), #s{}, edge()}
-                                                    | nomatch.
-feed_current(Event,
-             State0 = #s{counter = Counter},
-             #p{ cont = Parser0
-               , label = Label
-               , parser_id = ParserId
-               }) ->
-  Edge = #{ incident => Counter
-          , label => Label
-          , function => Parser0
-          , parser_id => ParserId
-          },
-  try (Parser0(Event))(State0) of
-    #stateful{state = State, val = {done, Result}} ->
-      %% Parser completed:
-      Done = {ParserId, Result},
-      {done, Done, State, Edge};
-    #stateful{state = State, val = {more, NextLabel, Parser}} ->
-      %% Parser waits for the next token:
-      More = #p{ cont = Parser
-               , label = NextLabel
-               , prev_vertex = Counter
-               , parser_id = ParserId
-               },
-      {more, More, State, Edge}
+-spec feed_current(snabbkaffe:event(), state(), m(state(), _Ret)) -> {boolean(), state()}.
+feed_current(Event, S0, #p{cont = Parser0, parser_id = ParserId}) ->
+  try (Parser0({Parser0, Event}))(S0) of
+    #stateful{state = S, val = {done, Result}} ->
+      %% Parser consumed all the token it needed:
+      {true, S#s{ active = zip_remove(S#s.active)
+                , done   = [Result | S#s.done]
+                }};
+    #stateful{state = S, val = {more, Parser}} ->
+      %% Parser needs more tokens:
+      {true, P1} = zip_get(S#s.active),
+      P = P1#p{cont = Parser},
+      {true, S#s{ active = zip_replace(P, S#s.active)
+                }}
   catch
-    {nomatch, _Nomatch} ->
-      nomatch;
+    {nomatch, _Event} ->
+      {false, S0};
     EC:Err:Stack ->
-      {fail, {ParserId, {EC, Err, Stack}}, State0, Edge}
+      {true, S0#s{ active = zip_remove(S0#s.active)
+                 , failed = [{ParserId, {EC, Err, Stack}} | S0#s.active]
+                 }}
   end.
 
 -spec add_edge(vertex_id(), edges(), edge()) -> edges().
@@ -349,6 +317,9 @@ add_edge(Emanent, Edges, Edge) ->
                    fun(EE) -> [Edge|EE] end,
                    [Edge],
                    Edges).
+
+cont_error(State) ->
+  error({cont_error, State}).
 
 %%================================================================================
 %% Testcases
@@ -376,49 +347,49 @@ bind_001_test() ->
 
 bind_010_test() ->
   M = [do/?MODULE ||
-        A <- next(label1),
+        A <- consume(),
         return(A)],
   S0 = state0,
   Event0 = event1,
-  #stateful{state = S0, val = {more, label1, M1}} = M(S0),
+  #stateful{state = S0, val = {more, M1}} = M(S0),
   ?assertMatch(#stateful{val = {done, Event0}, state = S0},
                (M1(Event0))(S0)).
 
 bind_020_test() ->
   M = [do/?MODULE ||
-        A <- next(label1),
+        A <- consume(),
         C <- return(1),
         X = A + C,
-        B <- next(),
+        B <- consume(),
         return({X, B})],
   S0 = 0,
-  #stateful{state = S1, val = {more, label1, M1}} = M(S0),
-  #stateful{state = S2, val = {more, undefined, M2}} = (M1(3))(S1),
+  #stateful{state = S1, val = {more, M1}} = M(S0),
+  #stateful{state = S2, val = {more, M2}} = (M1(3))(S1),
   #stateful{state = S3, val = {done, Ret}} = (M2(2))(S1),
   ?assertMatch({4, 2}, Ret),
   ?assertMatch(S0, S3).
 
 bind_030_test() ->
   M = [do/?MODULE ||
-        A <- next(label1),
+        A <- consume(),
         put_state([A]),
-        B <- next(),
+        B <- consume(),
         S <- get_state(),
         put_state([B|S]),
         return(ok)],
   S0 = [],
-  #stateful{state = S0, val = {more, label1, M1}} = M(S0),
-  #stateful{state = S1 = [evt1], val = {more, undefined, M2}} = (M1(evt1))(S0),
+  #stateful{state = S0, val = {more, M1}} = M(S0),
+  #stateful{state = S1 = [evt1], val = {more, M2}} = (M1(evt1))(S0),
   #stateful{state = S2 = [evt2, evt1], val = {done, ok}} = (M2(evt2))(S1).
 
 bind_040_test() ->
   M = [do/?MODULE ||
-        #{a := A} <- next(),
-        #{b := A} <- next(),
+        #{a := A} <- consume(),
+        #{b := A} <- consume(),
         return(A)],
   S0 = undefined,
-  #stateful{state = S0, val = {more, undefined, M1}} = M(S0),
-  #stateful{state = S0, val = {more, undefined, M2}} = (M1(#{a => 1}))(S0),
+  #stateful{state = S0, val = {more, M1}} = M(S0),
+  #stateful{state = S0, val = {more, M2}} = (M1(#{a => 1}))(S0),
   %% Matched the pattern:
   #stateful{state = S0, val = {done, 1}} = (M2(#{b => 1}))(S0),
   %% Didn't match the pattern:
