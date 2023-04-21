2a701a56358dc85a953320948638969f8b91afef
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2023-04-21 20:31:59 +0200

    Now it works, except for fork

--- src/snabbkaffe_parser.erl
+++ src/snabbkaffe_parser.erl
@@ -21,7 +21,10 @@
 -export([next/0, fork/0]).
 -export([new/1, feed/2, parse/2]).
 
--export_type([m/2, state/0, failed/0, parse_result/0, vertex_id/0, edge/0, edges/0]).
+%% Behavior callbacks:
+-export([return/1, bind/2, nomatch/1]).
+
+-export_type([m/1, state/1, failed/0, parse_result/1, vertex_id/0, edge/0, edges/0]).
 
 -compile({parse_transform, emonad}).
 
@@ -50,8 +53,8 @@
 
 -type edges() :: #{vertex_id() => [edge()]}.
 
--type parse_result() ::
-        #{ complete := [{parser_id(), term()}]
+-type parse_result(Result) ::
+        #{ complete := [{parser_id(), Result}]
          , incomplete := [parser_id()]
          , failed := [failed()]
          , vertices := #{vertex_id() => map()}
@@ -67,15 +70,30 @@
 
 %% Global state:
 -record(s,
-        { counter = 1 :: vertex_id()
-        , vertices = #{} :: #{vertex_id() => map()}
-        , edges = #{} :: edges()
-        , active = {[], []} :: zipper(#p{})
-        , done = [] :: [{vertex_id(), term()}]
-        , failed = [] :: [#p{}]
+        { seed_parser
+        , counter = 1
+        , vertices = #{}
+        , edges = #{}
+        , active = {[], []}
+        , done = []
+        , failed = []
         }).
 
--opaque state() :: #s{}.
+-opaque state(Result) ::
+          #s{ seed_parser :: fun((snabbkaffe:event(), state(Result)) ->
+                                    emonad_state:ret(snabbkaffe:event(), state(Result), Result))
+            , counter :: vertex_id()
+            , vertices :: #{vertex_id() => map()}
+            , edges :: edges()
+            , active :: zipper(#p{})
+            , done :: [{parser_id(), Result}]
+            , failed :: [failed()]
+            }.
+
+-type m(Result) :: emonad_state:t(snabbkaffe:event(), state(Result), Result).
+
+-type cont(Result) :: fun((snabbkaffe:event(), state(Result)) ->
+                             emonad_state:ret(snabbkaffe:event(), state(Result), Result)).
 
 %%================================================================================
 %% API funcions
@@ -83,58 +101,68 @@
 
 %% @doc Suspend execution of the parser until the next token is available.
 %% Return a continuation and a label
--spec next(label()) -> m(state(), snabbkaffe:event()).
+-spec next(label()) -> m(snabbkaffe:event()).
 next(Label) ->
-  [do/emonad_state ||
-    E <- emonad_state:consume(),
-    emonad_state:return(E)].
-  %% consume().
+  bind(
+    emonad_state:consume(),
+    fun(Evt) ->
+        bind(
+          emonad_state:get_state(),
+          fun(State) ->
+              get_cont(
+                fun(Cont) ->
+                    bind(
+                      emonad_state:put_state(on_match(Cont, Label, State)),
+                      fun(_) ->
+                          return(Evt)
+                      end)
+                end)
+          end)
+    end).
   %% [do/?MODULE ||
   %%   %% Cont <- get_cont(),
-  %%   Evt <- consume(),
+  %%   Evt <- emonad_state:consume(),
   %%   %% FIXME: we first update the state (which can be a rather heavy
   %%   %% operation), and only then check if event actually matches. This
   %%   %% should be optimized.
-  %%   %% over_state(fun(State) ->
-  %%   %%                {undefined, on_match(Cont, Label, State)}
-  %%   %%            end),
+  %%   State <- emonad_state:get_state(),
+  %%   emonad_state:put_state(on_match(foo, Label, State)),
   %%   return(Evt)].
 
 %% @equiv next(undefined)
--spec next() -> m(_S, snabbkaffe:event()).
+-spec next() -> m(snabbkaffe:event()).
 next() ->
   next(undefined).
 
 %% @doc Clone the parser.
--spec fork() -> m(_S, undefined).
+-spec fork() -> m(undefined).
 fork() ->
-  [do/emonad_state ||
-    S = #s{active = Active0} <- emonad_state:get_state(),
+  [do/?MODULE ||
+    S = #s{active = Active0, counter = Counter} <- emonad_state:get_state(),
     {true, P0 = #p{parser_id = Id}} = zip_get(Active0),
-    P = P0#p{parser_id = [make_ref() | Id]},
+    P = P0#p{parser_id = [Counter | Id]},
     Active = zip_insertl(P0, zip_replace(P, Active0)),
     emonad_state:put_state(S#s{active = Active})].
 
--spec parse(m(state(), _Result), [snabbkaffe:event()]) -> parse_result().
+-spec parse(m(Result), [snabbkaffe:event()]) -> parse_result(Result).
 parse(Parser, Trace) ->
   complete(lists:foldl(fun feed/2, new(Parser), Trace)).
 
--spec new(_SeedParser :: m(state(), _Result)) -> state().
+-spec new(m(Result)) -> state(Result).
 new(Parser) ->
-  Seed = [do/?MODULE || fork(), Parser],
-  {more, State, Cont} =
-  #stateful{state = S, val = {more, Cont}} = Seed(#s{}),
-  P = #p{ parser_id = []
-        , cont      = Cont
-        },
-  #s{active = zip_from_list([P])}.
+  #s{seed_parser = Parser}.
 
 %% @doc Feed next event to the parser
--spec feed(snabbkaffe:event(), state()) -> state().
-feed(Event, S0 = #s{counter = Incident, vertices = Vertices}) ->
-  {Matched, S1} = do_feed(false, Event, S0),
-  S = S1#s{ counter = Incident + 1
-          , active  = zip_from_list(zip_to_list(S1#s.active))
+-spec feed(snabbkaffe:event(), state(Result)) -> state(Result).
+feed(Event, S0 = #s{counter = Incident, vertices = Vertices, seed_parser = Seed}) ->
+  %% Seed the parser:
+  {more, S1, SeedCont} = emonad_state:run_state(Seed, S0),
+  SeedP = #p{parser_id = [Incident], cont = SeedCont},
+  S2 = S1#s{active = zip_insertr(SeedP, S1#s.active)},
+  %% Feed event to the active parsers:
+  {Matched, S3} = do_feed(false, Event, S2),
+  S = S3#s{ counter = Incident + 1
+          , active  = zip_from_list(zip_to_list(S3#s.active))
           },
   case Matched of
     true  -> S#s{vertices = Vertices#{Incident => Event}};
@@ -142,14 +170,27 @@ feed(Event, S0 = #s{counter = Incident, vertices = Vertices}) ->
   end.
 
 %% @doc Finish parsing
--spec complete(state()) -> parse_result().
+-spec complete(state(Result)) -> parse_result(Result).
 complete(#s{active = Active, done = Done, failed = Failed, vertices = Vertices, edges = Edges}) ->
-  #{ complete => Done
+  #{ complete   => Done
    , incomplete => [ParserId || #p{parser_id = ParserId} <- zip_to_list(Active)]
-   , failed => Failed
-   , vertices => Vertices
-   , edges => Edges
+   , failed     => Failed
+   , vertices   => Vertices
+   , edges      => Edges
    }.
+
+%%================================================================================
+%% Behavior callbacks
+%%================================================================================
+
+bind(A, N) ->
+  emonad_state:bind(A, N).
+
+return(A) ->
+  emonad_state:return(A).
+
+nomatch(A) ->
+  emonad_state:nomatch(A).
 
 %%================================================================================
 %% Zippers
@@ -191,70 +232,56 @@ zip_to_list({L, R}) ->
   lists:reverse(L) ++ R.
 
 %%================================================================================
-%% Internal exports
-%%================================================================================
-
-%%================================================================================
 %% Internal functions
 %%================================================================================
 
-%% @private Low level API that returns parser state
--spec get_state() -> m(S, S).
-get_state() ->
-  fun(S) ->
-      #stateful{val = {done, S}, state = S}
-  end.
+get_cont(Next) ->
+  bind(return(Next), Next).
 
-%% @private Low level API that modifies parser state
--spec put_state(S) -> m(S, S).
-put_state(S) ->
-  fun(_) ->
-      #stateful{val = {done, S}, state = S}
-  end.
-
--spec over_state(fun((S) -> {A, S})) -> m(S, A).
-over_state(Fun) ->
-  fun(S0) ->
-      {Val, State} = Fun(S0),
-      #stateful{val = {done, Val}, state = State}
-  end.
-
--spec do_feed(boolean(), snabbkaffe:event(), state()) -> {boolean(), state()}.
+-spec do_feed(boolean(), snabbkaffe:event(), state(Result)) -> {boolean(), state(Result)}.
 do_feed(Matched, Event, S0 = #s{active = Active0}) ->
   case zip_get(Active0) of
     false ->
       %% Reached the end of the zipper:
       {Matched, S0};
     {true, P0} ->
-      {MatchedThis, S1} = feed_current(Event, S0, P0),
-      S = S1#s{active = zip_shiftr(S1#s.active)},
+      {MatchedThis, S} = feed_current(Event, S0, P0),
       do_feed(Matched orelse MatchedThis, Event, S)
   end.
 
--spec feed_current(snabbkaffe:event(), state(), m(state(), _Ret)) -> {boolean(), state()}.
-feed_current(Event, S0, #p{cont = Parser0, parser_id = ParserId}) ->
-  try (Parser0(Event))(S0) of
-    #stateful{state = S, val = {done, Result}} ->
-      %% Parser consumed all the token it needed:
+-spec feed_current(snabbkaffe:event(), state(Result), cont(Result)) -> {boolean(), state(Result)}.
+feed_current(Event, S0, #p{cont = Cont0, parser_id = ParserId, prev_vertex = PrevVertex}) ->
+  try emonad_state:feed(Cont0, S0, Event) of
+    {done, S, Result} ->
+      %% Parser consumed all the token it needed. Note: no `shiftr' is
+      %% needed, since we removed the current item:
       {true, S#s{ active = zip_remove(S#s.active)
                 , done   = [{ParserId, Result} | S#s.done]
                 }};
-    #stateful{state = S, val = {more, Parser}} ->
+    {more, S, Cont} ->
       %% Parser needs more tokens:
       {true, P1} = zip_get(S#s.active),
-      P = P1#p{cont = Parser},
-      {true, S#s{ active = zip_replace(P, S#s.active)
+      P = P1#p{cont = Cont},
+      {true, S#s{ active = zip_shiftr(zip_replace(P, S#s.active))
                 }}
   catch
+    {nomatch, _Event} when PrevVertex =:= undefined ->
+      %% This was seed parser, we need to remove it. No shiftr is
+      %% needed:
+      {false, S0#s{ active = zip_remove(S0#s.active)
+                  }};
     {nomatch, _Event} ->
-      {false, S0};
+      %% This was a matured parser, we keep it and we need to shiftr:
+      {false, S0#s{ active = zip_shiftr(S0#s.active)
+                  }};
     EC:Err:Stack ->
+      %% Parser crashed. Add it to the list of exceptions. No shiftr is needed.
       {true, S0#s{ active = zip_remove(S0#s.active)
-                 , failed = [{ParserId, {EC, Err, Stack}} | S0#s.active]
+                 , failed = [{ParserId, {EC, Err, Stack}} | S0#s.failed]
                  }}
   end.
 
--spec on_match(m(state(), _Ret), label(), state()) -> state().
+-spec on_match(m(Result), label(), state(Result)) -> state(Result).
 on_match(Cont, Label, State = #s{active = Active, counter = Incident, edges = Edges}) ->
   {true, #p{ parser_id = ParserId
            , prev_vertex = Emanent
@@ -264,7 +291,7 @@ on_match(Cont, Label, State = #s{active = Active, counter = Incident, edges = Ed
           , parser_id => ParserId
           , function => Cont
           },
-  P = P0#p{cont = Cont, prev_vertex = Incident},
+  P = P0#p{prev_vertex = Incident, cont = Cont},
   State#s{ edges  = add_edge(Emanent, Edges, Edge)
          , active = zip_replace(P, Active)
          }.
@@ -276,163 +303,55 @@ add_edge(Emanent, Edges, Edge) ->
                    [Edge],
                    Edges).
 
-cont_error(State) ->
-  error({cont_error, State}).
-
 %%================================================================================
 %% Testcases
 %%================================================================================
 
 -ifdef(TEST).
 
-bind_000_test() ->
-  M = [do/?MODULE ||
-        return(1),
-        return(2)],
-  S0 = state0,
-  ?assertMatch(#stateful{state = S0, val = {done, 2}}, M(S0)).
-
-bind_001_test() ->
-  M = [do/?MODULE ||
-        S0 <- get_state(),
-        put_state(1),
-        S1 <- get_state(),
-        put_state(2),
-        S2 <- get_state(),
-        return({S0, S1, S2})],
-  S0 = 0,
-  ?assertMatch(#stateful{state = 2, val = {done, {0, 1, 2}}}, M(S0)).
-
-bind_010_test() ->
-  M = [do/?MODULE ||
-        A <- consume(),
-        return(A)],
-  S0 = state0,
-  Event0 = event1,
-  #stateful{state = S0, val = {more, M1}} = M(S0),
-  ?assertMatch(#stateful{val = {done, Event0}, state = S0},
-               (M1(Event0))(S0)).
-
-bind_020_test() ->
-  M = [do/?MODULE ||
-        A <- consume(),
-        C <- return(1),
-        X = A + C,
-        B <- consume(),
-        return({X, B})],
-  S0 = 0,
-  #stateful{state = S1, val = {more, M1}} = M(S0),
-  #stateful{state = S2, val = {more, M2}} = (M1(3))(S1),
-  #stateful{state = S3, val = {done, Ret}} = (M2(2))(S1),
-  ?assertMatch({4, 2}, Ret),
-  ?assertMatch(S0, S3).
-
-bind_021_test() ->
-  %% Test nesting of monadic expressions:
-  Rec = [do/?MODULE ||
-          E <- consume(),
-          return(E)],
-  M = [do/?MODULE ||
-        A <- Rec,
-        return(A)],
-  S0 = 0,
-  Event = foo,
-  #stateful{state = S0, val = {more, M1}} = M(S0),
-  #stateful{state = S0, val = {done, Ret}} = (M1(Event))(S0),
-  ?assertEqual(Event, Ret).
-
-bind_030_test() ->
-  M = [do/?MODULE ||
-        A <- consume(),
-        put_state([A]),
-        B <- consume(),
-        S <- get_state(),
-        put_state([B|S]),
-        return(ok)],
-  S0 = [],
-  #stateful{state = S0, val = {more, M1}} = M(S0),
-  #stateful{state = S1 = [evt1], val = {more, M2}} = (M1(evt1))(S0),
-  #stateful{state = S2 = [evt2, evt1], val = {done, ok}} = (M2(evt2))(S1).
-
-bind_040_test() ->
-  M = [do/?MODULE ||
-        #{a := A} <- consume(),
-        #{b := A} <- consume(),
-        return(A)],
-  S0 = undefined,
-  #stateful{state = S0, val = {more, M1}} = M(S0),
-  #stateful{state = S0, val = {more, M2}} = (M1(#{a => 1}))(S0),
-  %% Matched the pattern:
-  #stateful{state = S0, val = {done, 1}} = (M2(#{b => 1}))(S0),
-  %% Didn't match the pattern:
-  ?assertThrow({nomatch, #{b := 2}}, (M2(#{b => 2}))(S0)).
-
-bind_050_test() ->
-  M = [do/?MODULE ||
-        Cont <- get_cont(),
-        true = is_function(Cont, 1),
-        A <- consume(),
-        return(1)],
-  S0 = undefined,
-  #stateful{state = S0, val = {more, M1}} = M(S0),
-  #stateful{state = S0, val = {done, 1}} = (M1(#{a => 1}))(S0).
-
-feed_current_010_test() ->
-  M = [do/?MODULE ||
-        A <- consume(),
-        B <- consume(),
-        return({result, A, B})],
-  %% Create new state:
-  S0 = new(M),
-  {true, P0} = zip_get(S0#s.active),
-  %% Feed the first event:
-  {true, S1} = feed_current(foo, S0, P0),
-  %% Feed the second event:
-  {true, P1} = zip_get(S1#s.active),
-  {true, S2} = feed_current(bar, S1, P1),
-  %% Check the results:
-  ?assertMatch(
-     #s{ done = [{_ParserId, {result, foo, bar}}]
-       , active = {[], []}
-       },
-     S2).
-
-feed_current_020_test() ->
-  M = [do/?MODULE ||
-        A <- next(foo),
-        B <- next(bar),
-        return({result, A, B})],
-  %% Create new state:
-  S0 = new(M),
-  {true, P0} = zip_get(S0#s.active),
-  %% Feed the first event:
-  {true, S1} = feed_current(foo, S0, P0),
-  %% Feed the second event:
-  {true, P1} = zip_get(S1#s.active),
-  {true, S2} = feed_current(bar, S1, P1),
-  %% Check the results:
-  ?assertMatch(
-     #s{ done = [{_ParserId, {result, foo, bar}}]
-       , active = {[], []}
-       },
-     S2).
-
 feed_010_test() ->
   Seed = [do/?MODULE ||
-           #{a := A} <- consume(),
-           #{b := A} <- consume(),
-           return({pair, A})],
+           A <- emonad_state:consume(),
+           B <- emonad_state:consume(),
+           return({pair, A, B})],
   S0 = new(Seed),
-  %% %% This event doesn't match the seed parser:
-  %% S1 = feed(#{b => 1}, S0),
-  %% This event matches the seed parser, add new parser to the active set
-  S2 = feed(#{a => 1}, S0),
-  S3 = feed(#{b => 1}, S2),
-  ?assertMatch(#{ complete := [{[_Ref], {pair, 1}}]
+  S2 = feed(a, S0),
+  S3 = feed(b, S2),
+  ?assertMatch(#{ complete := [{[1], {pair, a, b}}]
+                , failed   := []
+                , vertices := #{1 := _, 2 := _}
+                },
+               complete(S3)).
+
+feed_011_test() ->
+  Seed = [do/?MODULE ||
+           A <- next(a),
+           B <- next(b),
+           return({pair, A, B})],
+  S0 = new(Seed),
+  S2 = feed(a, S0),
+  S3 = feed(b, S2),
+  ?assertMatch(#{ complete := [{[1], {pair, a, b}}]
+                , failed   := []
+                , vertices := #{1 := _, 2 := _}
+                , edges    := #{1 := _}
+                },
+               complete(S3)).
+
+feed_012_test() ->
+  Seed = [do/?MODULE ||
+           #{a := A} <- next(a),
+           #{b := B} <- next(b),
+           return({pair, A, B})],
+  S0 = new(Seed),
+  S1 = feed(#{a => a}, S0),
+  S2 = feed(#{x => x}, S1),
+  S3 = feed(#{b => b}, S2),
+  ?assertMatch(#{ complete   := [{[1], {pair, a, b}}]
                 , incomplete := []
-                , failed := []
-                , vertices := #{}
-                , edges := #{2 := [_]}
+                , failed     := []
+                , vertices   := #{1 := _, 3 := _}
+                , edges      := #{1 := _}
                 },
                complete(S3)).
 
@@ -442,7 +361,7 @@ parse_010_test() ->
            #{b := A} <- next(effect),
            return({pair, A})],
   Events = [#{c => 1}, #{b => 2}, #{a => 1}, #{a => 2}, #{c => 1}, #{b => 2}, #{b => 1}, #{a => 3}],
-  ?assertMatch(#{ complete := [{[_Ref1], {pair, 1}}, {[_Ref2], {pair, 2}}]
+  ?assertMatch(#{ complete := [{[3], {pair, 1}}, {[4], {pair, 2}}]
                 , incomplete := [_]
                 , failed := []
                 }, parse(Seed, Events)).
@@ -454,7 +373,9 @@ parse_020_fail_test() ->
   Events = [#{c => 1}, #{a => 1}, #{a => 2}],
   ?assertMatch(#{ complete := []
                 , incomplete := []
-                , failed := [_, _]
+                , failed := [ {[3], {error, deliberate, _}}
+                            , {[2], {error, deliberate, _}}
+                            ]
                 }, parse(Seed, Events)).
 
 parse_030_fork_test() ->
